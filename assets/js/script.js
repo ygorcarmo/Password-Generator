b478f9b5dc2340c16bc6f1534c150a4574c18221
Ygor <ygor.carmo@gmail.com>
2021-09-05 18:20:56 +1000

    update script.js, code works

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,45 +2,109 @@
 var generateBtn = document.querySelector("#generate");
 
 // Ammount of characters
-var characterAmout = function(){
-  characterAmoutRange = window.prompt("Enter Number Of Characters:");
+var characterAmount = function(){
+  characterAmountRange = window.prompt("Enter Number Of Characters:");
 
 // If user enter a value that is less than 8 or greater than 128
-
-if(characterAmoutRange < 8 || characterAmoutRange > 128) {
+if(characterAmountRange < 8 || characterAmountRange > 128) {
   window.alert("The character ammount must be greater than 8 and less than 128");   
 }
 };
 
+// Function with pass options
 var passOptions = function(){
 // Include UPPERCASE
-var inclueUpperCase = window.confirm("Include Uppercase ?");
-
+includeUpperCase = window.confirm("Include Uppercase ?");
 
 // Include Numbers
-var includeNumbers = window.confirm("Include Numbers?");
+includeNumbers = window.confirm("Include Numbers?");
 
 // Include Symbols
-var includeSymbols = window.confirm("Include Symbols?");
-}
-// Prompts when button selected
+includeSymbols = window.confirm("Include Symbols?");
+};
 
-generateBtn.addEventListener('click', function() {
-  characterAmout();
-  console.log(characterAmoutRange);
-  passOptions();
-  console.log(passOptions.includeNumbers);
-});
+// Character codes for lowercase which will be default
+var LOWERCASE_CHAR_CODES = arrayFromLowtoHigh(97, 122);
+
+// Character codes for UPPERCASE
+var UPPERCASE_CHAR_CODES = arrayFromLowtoHigh(65, 90);
+
+// Character codes for numbers
+var NUMBER_CHAR_CODES = arrayFromLowtoHigh(48, 57);
+
+// Character codes for Symbols
+var SYMBOL_CHAR_CODES = arrayFromLowtoHigh(33, 47).concat(
+  arrayFromLowtoHigh(58, 64)).concat(
+    arrayFromLowtoHigh(91, 96)).concat(
+      arrayFromLowtoHigh(123, 126)
+    );
+
+
+// Code for Prompts when button selected
+function generatePassword(){
+
+  characterAmount();
+  console.log(characterAmountRange);
+  if(characterAmountRange >= 8 && characterAmountRange <= 128){
+    passOptions();
+  }
+  if(!includeNumbers && !includeSymbols && !includeUpperCase){
+    window.alert("Password needs at least one of the options(Uppercase, Number or Symbol)");
+    passwordCharacters = "Try Again!"
+  }
+
+  // Generating pass
+  var charCodes = LOWERCASE_CHAR_CODES;
+
+// If include upper is true = add
+  if(includeUpperCase){
+    charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
+  }
+
+  // If include number is true = add
+  if(includeNumbers){
+    charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+  }
+
+  // If include symbol is true = add
+  if(includeSymbols){
+    charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
+  }
+
+  var passwordCharacters = []
+  // loop to get pass
+  for(var i = 0; i < characterAmountRange; i++){
+    // code to get random characters
+    var characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+    passwordCharacters.push(String.fromCharCode(characterCode))
+  };
+  console.log(passwordCharacters);
+
+  return passwordCharacters.join('');
+  
+  
+};
+
+// Array that will go through letters/numbers/symbols
+function arrayFromLowtoHigh(low, high){
+  var array= []
+  // for loop that will go through the numbers
+  for (let i = low; i <= high; i++){
+    array.push(i)
+  }
+  return array
+}
 
 
 // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-//   passwordText.value = password;
+  passwordText.value = password;
+  
+  
+}
 
-// }
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
